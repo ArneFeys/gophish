9b87fab86fb98076b2a82c547044cba1accb3729
Arne Feys <arne.feys@outlook.com>
2026-07-31 13:26:18 +0200

    new scan

--- util/util.go
+++ util/util.go
@@ -30,6 +30,7 @@ var (
 	positionRegex  = regexp.MustCompile(`(?i)position`)
 )
 
+// test new commit
 // ParseMail takes in an HTTP Request and returns an Email object
 // TODO: This function will likely be changed to take in a []byte
 func ParseMail(r *http.Request) (email.Email, error) {
